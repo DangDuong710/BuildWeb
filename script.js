f6160668b5fbac6b8459b80fac21a45772aec562
DangDuong710 <duongsoaimcb0@gmail.com>
2024-10-04 06:24:44 +0700

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function calculate() {
-    const num1 = parseFloat(document.getElementById('num1').value);
-    const num2 = parseFloat(document.getElementById('num2').value);
-    const result = num1 + num2;
-    document.getElementById('result').textContent = "Result: " + result;
-}
